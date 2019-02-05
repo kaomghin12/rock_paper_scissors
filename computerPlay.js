7b62fda64e9b4bee1cef1919b8e954e2ceb53c86
Kevin MP Guest <kevinpguest@gmail.com>
2019-02-04 16:22:46 -0800

    Finish and Test computerPlay() function.

--- computerPlay.js
+++ computerPlay.js
@@ -1,3 +1,14 @@
 function computerPlay() {
-    let choice = 
+    let random = Math.floor(Math.random() * 3) + 1;
+    let choice;
+
+    if (random === 1) {
+        choice = 'rock';
+    } else if (random === 2) {
+        choice = 'paper';
+    } else {
+        choice = 'scissors'
+    }
+
+    return choice;
 }
